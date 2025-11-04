4e08b7dbb72393a4ba043428853523eea8b3f028
Ming Shen <mingshen@Mings-MacBook-Pro.local>
2025-11-04 18:26:18 -0500

    Fix: Remove build step for Railway backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    return {
-      root: 'public',
-      base: '/Chinese-Lyrics-Generator/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, 'src'),
-        }
-      },
-      build: {
-        outDir: '../docs',
-        emptyOutDir: true
-      }
-    };
+export default defineConfig({
+  root: 'public',
+  base: '/Chinese-Lyrics-Generator/',
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
 });
